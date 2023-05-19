ea46771854b8740c9569cb098c7ddabce275354b
TuanB+UyenH <tuan.bui+uyen.huynh@eastagile.com>
2023-05-19 18:12:52 +0700

    WIP

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,24 +1,22 @@
-import { SiteStoreType } from './../types/stores/SiteStore';
+import { SiteStoreType } from "./../types/stores/SiteStore";
 import axios, { AxiosRequestConfig } from "axios";
 import { AuthStoreType } from "../types/stores/AuthStore";
 
 const requestInterceptorConfig = (config: AxiosRequestConfig) => {
   const persistedAuth = localStorage.getItem("authStore");
 
+  console.log("Call to:", config.url);
+
   if (persistedAuth) {
     const authStore: AuthStoreType = JSON.parse(persistedAuth);
-    config.headers[
-      "Authorization"
-    ] = `Bearer ${authStore.currentUser?.token}`;
+    config.headers["Authorization"] = `Bearer ${authStore.currentUser?.token}`;
   }
 
   const persistedSite = localStorage.getItem("siteStore");
 
   if (persistedSite) {
     const siteStore: SiteStoreType = JSON.parse(persistedSite);
-    config.headers[
-      "Root-Domain"
-    ] = siteStore.currentSite?.rootDomain;
+    config.headers["Root-Domain"] = siteStore.currentSite?.rootDomain;
   }
 
   return config;
@@ -29,12 +27,11 @@ export const request = {
     const instance = axios.create({
       baseURL: process.env.REACT_APP_API_URL,
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'System-Access-Key': process.env.REACT_APP_SYSTEM_ACCESS_KEY,
-        'Key-Inflection': 'camel',
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "System-Access-Key": process.env.REACT_APP_SYSTEM_ACCESS_KEY,
+        "Key-Inflection": "camel",
       },
-
     });
 
     instance.interceptors.request.use(requestInterceptorConfig);
